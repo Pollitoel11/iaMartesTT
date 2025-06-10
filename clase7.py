b2e5066c92b655c3b60a6c224109a19c1dc468d5
Rodrigo <rodripinocarranza@gmail.com>
2025-06-10 19:19:42 -0300

    Add files via upload
    primer commit

--- clase7.py
+++ clase7.py
@@ -79,4 +79,6 @@ def ejecutar_chat():
         agregar_mensajes_previos("assistant",respuesta_contenido)
         mostrar_mensaje("assistant",respuesta_contenido)
 
-
+# EJECUTAR LA APP( si __name__ es igual a __main__ se ejecuta la funcion, y __main__ es mi archivo principal)
+if __name__ == '__main__':
+    ejecutar_chat()
